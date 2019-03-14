cbe48139d95c80197f093a108b7e41c5b75832a3
Thomas Reto Strub <ths@strubt.ch>
2019-03-15 00:19:03 +0100

    bugfix Persist() isCircular

--- SchemaGenerator/SchemaNode.cs
+++ SchemaGenerator/SchemaNode.cs
@@ -49,7 +49,7 @@ namespace StrubT.SchemaGenerator {
 			if (string.IsNullOrEmpty(type)) ret.Add("schemaTypes", schema.AllSchemaTypes.ToList());
 			ret.Add("children", schema.ChildNodes
 				.Where(n => string.IsNullOrEmpty(type) || n.SchemaTypes.Contains(type))
-				.Select(n => SerializeSchema(n, new HashSet<SchemaNode> { n }, type)).ToList());
+				.Select(n => SerializeSchema(n, new HashSet<SchemaNode>(), type)).ToList());
 
 			return ret;
 		}
